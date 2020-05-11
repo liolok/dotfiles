26c04dcdfd9a6d92f2bad4170801c125bcddd29a
liolok <acer-sf@liolok.com>
2020-05-11 16:06:33 +0800

    fish: update key bindings

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,10 @@
 # (https://fishshell.com/docs/current/cmds/bind.html)
+# Simplest syntax: bind KEY ACTION(S)
+# KEY: Run `fish_key_reader` to get key sequence, which depends on termianls.
+# ACTIONS: (https://fishshell.com/docs/current/cmds/bind.html#special-input-functions)
 function fish_user_key_bindings
-    bind \cH backward-kill-word # Ctrl Backspace under Gnome terminal and Guake
-    bind \e\x7f backward-kill-bigword # Alt Backspace
+    # Tested on Gnome terminal and TTY
+    bind \e\x7F backward-kill-bigword # Alt Backspace
+    bind \e\[3\;3~ kill-bigword # Alt Delete
+    bind \e\r accept-autosuggestion execute # Alt Enter
 end
