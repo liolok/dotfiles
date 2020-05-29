1625bd4de8317777c7056b2bc5ea6ef617921160
liolok <acer-sf@liolok.com>
2020-05-29 13:34:59 +0800

    fish: disable neofetch in greeting

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,4 +1,4 @@
 # (https://fishshell.com/docs/current/faq.html#how-do-i-change-the-greeting-message)
 function fish_greeting
-    neofetch # disable due to low startup speed
+    # neofetch # disable to speedup start
 end
