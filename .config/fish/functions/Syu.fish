f7f3ed8032d63a55a18ce3f3120050911f57c37c
liolok <asus-e3@liolok.com>
2020-06-18 23:56:21 +0800

    fish: fix Syu function

--- .config/fish/functions/Syu.fish
+++ .config/fish/functions/Syu.fish
@@ -1,7 +1,7 @@
 function Syu --description "Upgrading the system and maybe install new packages"
     # (https://wiki.archlinux.org/index.php/Pacman#Upgrading_packages)
     sudo pacman -Syu
-    if test -n "$argv"; sudo pacman -S --needed "$argv"; end
+    if test -n "$argv"; sudo pacman -S --needed $argv; end
     # (https://wiki.archlinux.org/index.php/Pacman/Tips_and_tricks#Removing_unused_packages_(orphans))
     # sudo pacman -Rns (pacman -Qtdq) # outputs error if no orphans found
     # (https://github.com/farseerfc/dotfiles/blob/d4f353104bc4ade853c55ad9d8d3cd7d56bb7044/zsh/.bashrc#L62)
