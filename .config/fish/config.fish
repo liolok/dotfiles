e73b3094beaba2035db9715c3d433475efdbbee5
liolok <asus-e3@liolok.com>
2020-10-22 22:53:47 +0800

    fish: remove HOME and PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,6 @@
 # (https://wiki.archlinux.org/index.php/Fish)
 # (https://fishshell.com/docs/current/index.html)
 
-# Real user home, not applications $HOME like `/home/liolok/apps`.
-# If not exist, for example running as root, just fallback to $HOME.
-set HOME_USER /home/$USER; test -d $HOME_USER || set HOME_USER $HOME
-
-# PATH: directory search list of executable files
-# (https://fishshell.com/docs/current/tutorial.html#path)
-# set --prepend fish_user_paths $HOME_USER/scripts # my script directory
-
 # Powerline shell prompt
 # (https://powerline.readthedocs.io/en/latest/usage/shell-prompts.html#fish-prompt)
 set --append fish_function_path /usr/share/powerline/bindings/fish
