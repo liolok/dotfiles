6a575fd17c5121569855f58460ce7f708c8a999f
liolok <asus-e3@liolok.com>
2021-03-11 23:56:02 +0800

    fish: refactor fallback function

--- .config/fish/functions/fallback.fish
+++ .config/fish/functions/fallback.fish
@@ -1,5 +1,6 @@
 # (https://stackoverflow.com/questions/54524521/in-fish-shell-how-to-set-a-variable-with-default-fallback/59532292#59532292)
-function fallback --argument-names list --description "Fallback to first non-zero string in given list"
+function fallback --argument-names "list" \
+--description "Fallback to first non-zero string in given list"
     for value in $list
         if test -n $value # non-zero string
             echo $value # output
