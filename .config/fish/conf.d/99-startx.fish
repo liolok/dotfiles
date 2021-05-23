827c2b11994d53cd05881e331c8e613b55105968
liolok <asus-e3@liolok.com>
2021-05-24 01:17:58 +0800

    fish: add startx script

---
+++ .config/fish/conf.d/99-startx.fish
@@ -0,0 +1,6 @@
+# (https://wiki.archlinux.org/title/Fish#Start_X_at_login)
+status is-login; and test -z "$DISPLAY"; and test "$XDG_VTNR" = 1; or exit
+# (https://wiki.archlinux.org/title/Xorg#Session_log_redirection)
+set --local data_dir $XDG_DATA_HOME/xorg
+mkdir --parents $data_dir
+exec startx /usr/bin/startplasma-x11 &> $data_dir/session.log
