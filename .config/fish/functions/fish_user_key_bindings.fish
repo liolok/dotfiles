67cbee3d2c9edd624a82b2ff5f57696d033eecf3
liolok <asus-e3@liolok.com>
2021-05-30 01:52:49 +0800

    fish: update

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -3,7 +3,7 @@
 # KEY: Run `fish_key_reader` to get key sequence, which depends on termianls.
 # ACTIONS: (https://fishshell.com/docs/current/cmds/bind.html#special-input-functions)
 function fish_user_key_bindings
-    # Tested on Gnome terminal and TTY
+    # Tested on Gnome terminal, Konsole and TTY
     bind \e\x7F backward-kill-bigword # Alt Backspace
     bind \e\[3\;3~ kill-bigword # Alt Delete
     bind \e\r accept-autosuggestion execute # Alt Enter
