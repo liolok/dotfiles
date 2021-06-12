18dfe71e4bff313dcb7db040b8183532624ebeb4
liolok <asus-e3@liolok.com>
2021-06-12 23:15:01 +0800

    change fakehome from function to script

--- .config/fish/functions/fakehome.fish
+++
@@ -1,6 +0,0 @@
-function fakehome --description "Use dedicated home to run app"
-    set app (basename $argv[1]) # expect absolute path to app
-    set home (fallback $XDG_DATA_HOME ~/.local/share)/$app # dedicated home
-    mkdir --parents $home
-    HOME=$home $argv # passthrough all arguments
-end
