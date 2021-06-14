8d6081eb89088bb891305602671636c8daee6dab
liolok <asus-e3@liolok.com>
2021-06-15 00:35:19 +0800

    fish: fix typo

--- .config/fish/conf.d/50-powerline.fish
+++ .config/fish/conf.d/50-powerline.fish
@@ -2,7 +2,7 @@
 # (https://powerline.readthedocs.io/en/latest/usage/shell-prompts.html#fish-prompt)
 
 
-string match --quiet '*pts*' (tty)l or exit # only enable under pseudo terminal slaves
+string match --quiet '*pts*' (tty); or exit # only enable under pseudo terminal slaves
 set --local powerline_binding /usr/share/powerline/bindings/fish
 test -d "$powerline_binding"; or exit # skip if not installed
 set --append fish_function_path $powerline_binding
