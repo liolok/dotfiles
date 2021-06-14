4447c2cc9f347facb4ec4512ef913625876c0a50
liolok <asus-e3@liolok.com>
2021-06-14 19:21:04 +0800

    fish: make powerline optional

--- .config/fish/conf.d/01-powerline.fish
+++
@@ -1,7 +0,0 @@
-# (https://powerline.readthedocs.io/en/latest/usage/shell-prompts.html#fish-prompt)
-
-if string match --quiet '*pts*' (tty) # only enable under pseudo terminal slaves
-    set --local powerline_binding /usr/share/powerline/bindings/fish
-    set --append fish_function_path $powerline_binding
-    powerline-setup
-end
