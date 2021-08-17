cb1bc1ab7aa6fb3631a9d7eed95ad940829b20a5
liolok <asus-e3@liolok.com>
2021-08-17 22:08:00 +0800

    fish: remove powerline

--- .config/fish/conf.d/50-powerline.fish
+++
@@ -1,9 +0,0 @@
-# ~/.config/fish/conf.d/50-powerline.fish
-# (https://powerline.readthedocs.io/en/latest/usage/shell-prompts.html#fish-prompt)
-
-
-string match --quiet '*pts*' (tty); or exit # only enable under pseudo terminal slaves
-set --local powerline_binding /usr/share/powerline/bindings/fish
-test -d "$powerline_binding"; or exit # skip if not installed
-set --append fish_function_path $powerline_binding
-powerline-setup
