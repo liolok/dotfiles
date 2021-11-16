745685230d2bba466cf54053dd0cd72b813ab923
liolok <asus-e3@liolok.com>
2021-11-17 07:03:20 +0800

    fish: use fastfetch if available in fish_greeting

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,4 +1,4 @@
 # (https://fishshell.com/docs/current/faq.html#how-do-i-change-the-greeting-message)
 function fish_greeting
-    # neofetch # disable to speedup start
+    command --query fastfetch; and fastfetch
 end
