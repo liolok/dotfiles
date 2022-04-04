07f9fb2ea287664dbddcf7f49f7f828afc9799ef
liolok <asus-e3@liolok.com>
2022-04-05 07:34:55 +0800

    fish: add abbr only when interactive

--- .config/fish/conf.d/02-abbr.fish
+++ .config/fish/conf.d/02-abbr.fish
@@ -1,20 +1,36 @@
 # ~/.config/fish/conf.d/02-abbr.fish
 # (https://fishshell.com/docs/current/cmds/abbr.html)
 
+status is-interactive
+or exit
+
 # Use `-` as `cd -` to move to previous directory, for consistency with auto-cd.
 # (https://github.com/fish-shell/fish-shell/issues/709#issuecomment-261745982)
 a - 'cd -'
 
 a fh 'free --human'
 
-a nf 'neofetch'
+a nf neofetch
 
-a pm 'pacman'
+a pfb 'pkgfile --verbose --binaries'
+
+a pm pacman
 a pmq 'pacman --query'
+a pmqi 'pacman --query --info'
+a pmqo 'pacman --query --owns'
 a pmr 'pacman --remove'
 a pms 'pacman --sync'
 
-a sc 'systemctl'
+a sc systemctl
 a sce 'systemctl edit'
 a scr 'systemctl restart'
 a scs 'systemctl status'
+
+# Prepend sudo for needed command (if I'm not root and have sudo installed)
+if test (whoami) != root
+    and command --query sudo
+    a pmr 'sudo pacman --remove'
+    a pms 'sudo pacman --sync'
+    a sce 'sudo systemctl edit'
+    a scr 'sudo systemctl restart'
+end
