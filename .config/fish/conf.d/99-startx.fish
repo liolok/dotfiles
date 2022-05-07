6929fa154e9c48d5e6663f215ea0bf743731965a
liolok <asus-e3@liolok.com>
2022-05-07 14:25:17 +0800

    fish: update startx script

--- .config/fish/conf.d/99-startx.fish
+++ .config/fish/conf.d/99-startx.fish
@@ -1,8 +1,23 @@
 # ~/.config/fish/conf.d/99-startx.fish
+# https://wiki.archlinux.org/title/Xinit#Autostart_X_at_login
 # https://wiki.archlinux.org/title/Fish#Start_X_at_login
-
-status is-login; and test -z "$DISPLAY"; and test "$XDG_VTNR" = 1; or exit
+# https://wiki.archlinux.org/title/KDE#From_the_console
 # https://wiki.archlinux.org/title/Xorg#Session_log_redirection
-set --local data_dir $XDG_DATA_HOME/xorg
-mkdir --parents $data_dir
-exec startx /usr/bin/startplasma-x11 &> $data_dir/session.log
+# https://fishshell.com/docs/current/language#input-output-redirection
+
+if not status is-login # not login shell
+    or not command --query startx # xorg-xinit not installed
+    or string length --quiet $DISPLAY # X already started?
+    or not string length --quiet $XDG_VTNR # virtual terminal number not set
+    or test "$XDG_VTNR" -gt 1 # virtual terminal number greater than 1
+
+    exit
+end
+
+set --local data_home $XDG_DATA_HOME
+string length --quiet $data_home
+or set --local data_home ~/.local/share
+set --local data_xorg $data_home/xorg
+mkdir --parents $data_xorg
+
+exec startx /usr/bin/startplasma-x11 &> $data_xorg/session.log
